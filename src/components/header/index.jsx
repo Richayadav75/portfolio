08cb0817b775383f7c57f2c00961e2a02e12be0e
Richayadav75 <richayadav967096@gmail.com>
2020-08-28 21:22:20 +0530

    Initial commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+class Header extends Component {
+  //creating class
+  render() {
+    // creating render function
+    return (                     //return one view
+        <div>
+                <h1>hello</h1>
+        </div>
+    )
+  }
+}
+
+export default Header;              //we can retun this line no 3 also  export default class Header extends React.Component() {}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import Header from '../../components/header'
+
+export default class Home extends Component{
+    render(){
+        return (
+            <div>
+                <Header/>
+            </div>
+            
+        )
+    }
+
+}
