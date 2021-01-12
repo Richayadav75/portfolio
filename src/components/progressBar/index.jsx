740b070d62e79c143ab89a38ff30f16239bf9321
Richa Yadav <richa.yadav@techchefz.com>
2021-01-12 13:09:27 +0530

    Skill Added
    Skill Set added with progressBar

--- src/components/progressBar/index.jsx
+++ src/components/progressBar/index.jsx
@@ -1,23 +1,56 @@
-import React, { Component } from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import {Container, Row, Col } from 'react-bootstrap';
-import './style.css';
-
+import React, { Component } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import { Container, Row, Col } from "react-bootstrap";
+import "./style.css";
 
 export default class Progress extends Component {
-    render() {
-        return (
-            <div>
-                <section className="section">
-                    <Container>
-                        <Row>
-                            <Col>
-                                <ProgressBar className="" variant="success" now={60} max={100} min={0} key={1} />
-                            </Col>
-                        </Row>
-                    </Container>
-                </section>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <section className="section">
+          <Container>
+            <Row>
+              <Col xs="12" sm="12" lg="12" xl="12">
+                <h3 className="resume-title">{this.props.sName}</h3>
+              </Col>
+              <Col xs="6" sm="4" lg="6" xl="6">
+                {this.props.slist.map((listItem, index) => (
+                  <div className="progress-list" key={index}>
+                    <div className="d-flex justify-content-between progress-list__skill">
+                      <span className="">{listItem.skillName}</span>
+                      <span className="">{listItem.skillPercent}</span>
+                    </div>
+                    <ProgressBar
+                      className="progress-bar__skill"
+                      now={60}
+                      max={100}
+                      min={0}
+                      key={1}
+                    />
+                  </div>
+                ))}
+              </Col>
+              <Col xs="6" sm="4" lg="6" xl="6">
+                {this.props.slist.map((listItem, index) => (
+                  <div className="progress-list" key={index}>
+                    <div className="d-flex justify-content-between progress-list__skill">
+                      <span className="">{listItem.skillName}</span>
+                      <span className="">{listItem.skillPercent}</span>
+                    </div>
+                    <ProgressBar
+                      className="progress-bar__skill"
+                      now={60}
+                      max={100}
+                      min={0}
+                      key={1}
+                    />
+                  </div>
+                ))}
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </div>
+    );
+  }
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,51 +1,63 @@
-import React,{Component} from 'react';
-import Header from '../../components/header'
-import Banner from '../../components/banner';
-import Description from '../../components/description';
-import ResumeList from '../../components/resumeList';
-import Progress from '../../components/progressBar';
-import TabComponent from '../../components/tab';
-import CarouselComponent from '../../components/carousel';
+import React, { Component } from "react";
+import Header from "../../components/header";
+import Banner from "../../components/banner";
+import Description from "../../components/description";
+import ResumeList from "../../components/resumeList";
+import Progress from "../../components/progressBar";
+import TabComponent from "../../components/tab";
+import CarouselComponent from "../../components/carousel";
 
-export default class Home extends Component{
-    constructor(props) {   //Constructor own method 
-        super(props);   //override own constructor method 
-        this.state = {    //state initilization
-            title: "Education",   //state initization for title state
-            educationData: [{
-                instituteName: "UPTU",
-                startYear: 2006,
-                endYear: 2010,
-                courseName: "b.Tech"
-            },
-            {
-                instituteName: "S.k.d",
-                startYear: 2006,
-                endYear: 2010,
-                courseName: "b.Tech"
-            },
-            {
-                instituteName: "UPTU",
-                startYear: 2006,
-                endYear: 2010,
-                courseName: "b.Tech"
-            }]
-
-        }
-    }
-    render(){
-        return (
-            <div>
-                <Header/>
-                <Banner />
-                <Description />
-                <ResumeList name={this.state.title} list={this.state.educationData} />
-                <Progress />
-                <TabComponent />
-                <CarouselComponent />
-            </div>
-            
-        )
-    }
-
+export default class Home extends Component {
+  constructor(props) {
+    //Constructor own method
+    super(props); //override own constructor method
+    this.state = {
+      //state initilization
+      title: "Education", //state initization for title state
+      educationData: [
+        {
+          instituteName: "UPTU",
+          startYear: 2006,
+          endYear: 2010,
+          courseName: "b.Tech",
+        },
+        {
+          instituteName: "S.k.d",
+          startYear: 2006,
+          endYear: 2010,
+          courseName: "b.Tech",
+        },
+        {
+          instituteName: "UPTU",
+          startYear: 2006,
+          endYear: 2010,
+          courseName: "b.Tech",
+        },
+      ],
+      skillTitle: "General Skills",
+      skillSubTitle: [
+        { skillName: "HTML", skillPercent: "80%" },
+        { skillName: "CSS", skillPercent: "60%" },
+        { skillName: "JQuery", skillPercent: "50%" },
+        { skillName: "HTML", skillPercent: "80%" },
+        { skillName: "HTML", skillPercent: "80%" },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Banner />
+        <Description />
+        <ResumeList name={this.state.title} list={this.state.educationData} />
+        <Progress
+          sName={this.state.skillTitle}
+          slist={this.state.skillSubTitle}
+        />
+        <TabComponent />
+        <CarouselComponent />
+      </div>
+    );
+  }
 }
