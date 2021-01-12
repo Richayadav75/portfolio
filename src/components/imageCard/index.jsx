a4d5bb2901af73115db806ae0a6dde3b3290eac3
Richa Yadav <richa.yadav@techchefz.com>
2021-01-12 13:48:58 +0530

    Project description added

--- src/components/imageCard/index.jsx
+++ src/components/imageCard/index.jsx
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import { Card, Button } from 'react-bootstrap';
-
+import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
+import slider from "../../assets/images/img_project_1_mono.png";
+import "./style.css";
 export default class ImageCard extends Component {
-    render() {
-        return (
-            <div>
-                <Card>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>
-                            With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="border">
+        <Row>
+          <Col xs="6" sm="4" lg="6" xl="5">
+            <img className="d-block w-100" src={slider} alt="First slide" />
+          </Col>
+          <Col className="project-brief" xs="6" sm="4" lg="6" xl="7">
+            <h5 className="project-title">
+              Mobile and desktop app for London hostel store
+            </h5>
+            <p className="project-description">
+              Do answered bachelor occasion in of offended no concerns. Supply
+              worthy warmth branch of no ye. Voice tried known to as my to.
+              Though wished merits or be. Alone visit use these smart rooms ham.
+            </p>
+            <p className="project-stack">Used stack:</p>
+            <ul className="project-tech pl-0">
+              <li>HTML</li>
+              <li>css3</li>
+            </ul>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
--- src/components/tab/index.jsx
+++ src/components/tab/index.jsx
@@ -1,33 +1,58 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Container, Row, Col } from 'react-bootstrap';
-import ImageCard from '../imageCard';
-import './style.css';
+import React, { Component } from "react";
+import { Tabs, Tab, Container, Row, Col } from "react-bootstrap";
+import ImageCard from "../imageCard";
+import "./style.css";
 
 export default class TabComponent extends Component {
-    render() {
-        return (
-            <div>
-                <section className="section">
-                    <Container>
-                        <Row>
-                            <Col>
-                                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                                    <Tab eventKey="home" title="Home">
-                                        <ImageCard />
-                                    </Tab>
-                                    <Tab eventKey="profile" title="Profile">
-                                        <ImageCard />
-
-                                    </Tab>
-                                    <Tab eventKey="contact" title="Contact">
-                                        <ImageCard />
-                                    </Tab>
-                                </Tabs>
-                            </Col>
-                        </Row>
-                    </Container>
-                </section>
-            </div >
-        )
-    }
+  render() {
+    return (
+      <div>
+        <section className="section">
+          <Container>
+            <Row>
+              <Col>
+                <Tabs
+                  className="project-tabs"
+                  defaultActiveKey="home"
+                  id="uncontrolled-tab-example"
+                >
+                  <Tab
+                    className="project-tabs-list"
+                    eventKey="home"
+                    title="Home"
+                  >
+                    <div className="mb-3">
+                      <ImageCard />
+                    </div>
+                    <div className="mb-3">
+                      <ImageCard />
+                    </div>
+                    <div className="mb-3">
+                      <ImageCard />
+                    </div>
+                    <div className="mb-3">
+                      <ImageCard />
+                    </div>
+                  </Tab>
+                  <Tab eventKey="profile" title="Profile">
+                    <div className="mb-3">
+                      <ImageCard />
+                    </div>
+                    <div className="mb-3">
+                      <ImageCard />
+                    </div>
+                  </Tab>
+                  <Tab eventKey="contact" title="Contact">
+                    <div className="mb-3">
+                      <ImageCard />
+                    </div>
+                  </Tab>
+                </Tabs>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </div>
+    );
+  }
 }
