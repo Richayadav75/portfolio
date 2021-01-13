9f6b19522f9868f22d94186017ecb1eb9cfecbaa
Richa Yadav <richa.yadav@techchefz.com>
2021-01-13 16:40:22 +0530

    design fixes

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,12 +1,14 @@
-import React , { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 export default class ButtonComponent extends Component {
-    render(){
-        return(
-            <div>
-                <Button variant="primary">Primary</Button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Button onClick={this.props.onClickHandler} variant="primary">
+          Primary
+        </Button>
+      </div>
+    );
+  }
 }
--- src/components/description/index.jsx
+++ src/components/description/index.jsx
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
-import {Container, Row, Col } from 'react-bootstrap';
-import './index.css';
-import ButtonComponent from '../button';
-
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./index.css";
+import ButtonComponent from "../button";
 
 export default class Description extends Component {
-    render() {
-        return (
-            <div>
-                <section className="section">
-                    <Container>
-                        <Row>
-                            <Col>
-                                <h1 className="text-lg">
-                                    Hi_
-                                </h1>
-                                <p className="py-5 person-description">
-                                    I am Richa Web developer able to build a Web presence from the ground up - from concept, navigation, layout and programming to UX and SEO. Skilled at writing well-designed, testable and efficient code using current best practices in Web development. Fast learner, hard worker and team player who is proficient in an array of scripting languages and multimedia Web tools.
-                                </p>
-                                <ButtonComponent />
-                            </Col>
-                        </Row>
-                    </Container>
-                </section>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  isClickHandle = () => {
+    let state = this.state;
+    state.isOpen = !state.isOpen;
+    this.setState({ ...state });
+    console.log(state);
+  };
+  render() {
+    return (
+      <div>
+        <section className="section">
+          <Container>
+            <Row>
+              <Col>
+                <h1 className="text-lg">Hi_</h1>
+                <p className="py-5 person-description">
+                  I am Richa Web developer able to build a Web presence from the
+                  ground up - from concept, navigation, layout and programming
+                  to UX and SEO. Skilled at writing well-designed, testable and
+                  efficient code using current best practices in Web
+                  development. Fast learner, hard worker and team player who is
+                  proficient in an array of scripting languages and multimedia
+                  Web tools.
+                </p>
+                <ButtonComponent onClickHandler={this.isClickHandle} />
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </div>
+    );
+  }
 }
