7771fff107010982e54ada52775ea6d3e9f81e53
Richa Yadav <richa.yadav@techchefz.com>
2021-01-14 16:39:11 +0530

    carousel changes fixed

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,56 +1,69 @@
-import React, { Component } from 'react';
-import { Carousel, Container, Row, Col } from 'react-bootstrap';
-import slider from '../../assets/images/slide.jpg';
-import './style.css'
+import React, { Component } from "react";
+import { Carousel, Container, Row, Col } from "react-bootstrap";
+import slider from "../../assets/images/slide.jpg";
+import "./style.css";
 
 export default class CarouselComponent extends Component {
-    render() {
-        return (
-            <div>
-                <section className="section">
-                    <Container>
-                        <Row>
-                            <Carousel>
-                                <Carousel.Item>
-                                    <img
-                                        className="d-block w-100"
-                                        src={slider}
-                                        alt="First slide"
-                                    />
-                                    <Carousel.Caption>
-                                        <h3>First slide label</h3>
-                                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img
-                                        className="d-block w-100"
-                                        src={slider}
-                                        alt="Third slide"
-                                    />
+  render() {
+    return (
+      <div>
+        <section className="section">
+          <div
+            className=""
+            style={{ backgroundImage: "./assets/images/slide.jpg" }}
+          >
+            <Container>
+              <Row>
+                <Carousel>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={slider}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h3>First slide label</h3>
+                      <p>
+                        Nulla vitae elit libero, a pharetra augue mollis
+                        interdum.
+                      </p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={slider}
+                      alt="Third slide"
+                    />
 
-                                    <Carousel.Caption>
-                                        <h3>Second slide label</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img
-                                        className="d-block w-100"
-                                        src={slider}
-                                        alt="Third slide"
-                                    />
+                    <Carousel.Caption>
+                      <h3>Second slide label</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      </p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={slider}
+                      alt="Third slide"
+                    />
 
-                                    <Carousel.Caption>
-                                        <h3>Third slide label</h3>
-                                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                            </Carousel>
-                        </Row>
-                    </Container>
-                </section>
-            </div>
-        )
-    }
+                    <Carousel.Caption>
+                      <h3>Third slide label</h3>
+                      <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl
+                        consectetur.
+                      </p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
+              </Row>
+            </Container>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
